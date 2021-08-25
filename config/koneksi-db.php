2d07bb2bf0f27b41a686561ec5a52b461e6e03dc
Ihsan Sayid Muharram <61352696+ihsansayid99@users.noreply.github.com>
2021-08-26 00:27:47 +0700

    fix lah

--- config/koneksi-db.php
+++ config/koneksi-db.php
@@ -1,15 +1,15 @@
 <?php
 
-    $hostname   = "localhost";
-    $user       = "root";
-    $password   = "";
-    $db_name    = "dbpus";
-
+    $hostname   = "mysql-db";
+    $user       = "ihsan";
+    $password   = "ihsanpass";
+    $db_name    = "db_pus";
+    
     $db_conn = mysqli_connect($hostname, $user, $password, $db_name);
 
     if(!$db_conn){
         die("Gagal terhubung dengan database: " . mysqli_connect_error());
-    }
+    } 
 ?>
 
 
